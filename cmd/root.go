185df63d74112e0f88904ee678be661c93778257
t.akasaki <t.akasaki.csharp@gmail.com>
2025-02-02 17:19:34 +0900

    feat: :sparkles: ツールの説明文を設定

--- cmd/root.go
+++ cmd/root.go
@@ -88,17 +88,9 @@ func setSessionToken(credentials *types.Credentials) {
 	exec.Command("aws", "configure", "set", "mfa_serial", serialNumber, "--profile", outProfile).Output()
 }
 
-// rootCmd は、サブコマンドなしで呼び出された場合の基本コマンドを表します。
 var rootCmd = &cobra.Command{
 	Use:   "aws-cli-mfa-login",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// 次の行のコメントを解除すると、ベアアプリケーションに関連するアクションがある場合に実行されます
+	Short: "A tool for obtaining AWS CLI sessions using a virtual authenticator application.",
 	Run: func(cmd *cobra.Command, args []string) {
 
 		ctx := context.Background()
@@ -108,8 +100,6 @@ to quickly create a Cobra application.`,
 	},
 }
 
-// Execute は、すべての子コマンドをルートコマンドに追加し、フラグを適切に設定します。
-// これは main.main() によって呼び出されます。ルートコマンドに対して一度だけ実行すれば十分です。
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
@@ -118,16 +108,8 @@ func Execute() {
 }
 
 func init() {
-	// ここでは、フラグと設定を定義します。
-	// Cobra は永続フラグをサポートしており、ここで定義するとアプリケーション全体で有効になります。
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.aws-cli-mfa-login.yaml)")
-
-	// また、Cobra はローカルフラグもサポートしており、これはこのアクションが直接呼び出されたときにのみ有効になります。
 	rootCmd.Flags().StringVarP(&profile, "profile", "p", "default", "AWS profile name")
-	rootCmd.Flags().StringVarP(&serialNumber, "serial-number", "s", "", `MFA identifier. If not specified, the value of mfa_serial in AWS configure specified in the --out-profile argument is used.
-	Ex: arn:aws:iam::123456789012:mfa/user
-	`)
+	rootCmd.Flags().StringVarP(&serialNumber, "serial-number", "s", "", "MFA identifier. If not specified, the value of mfa_serial in AWS configure specified in the --out-profile argument is used.\nEx: arn:aws:iam::123456789012:mfa/user")
 	rootCmd.Flags().StringVarP(&outProfile, "out-profile", "o", "default-sts", "AWS profile name to be used in STS")
 	rootCmd.Flags().Int32VarP(&durationSeconds, "duration-seconds", "d", 3600, "The duration, in seconds, that the credentials should remain valid.")
 }
