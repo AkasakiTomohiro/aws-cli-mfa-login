6ee86b4c61feb886b23a40d3d08b408970ee31aa
t.akasaki <t.akasaki.csharp@gmail.com>
2025-02-04 22:52:48 +0900

    feat: :sparkles: MFA識別子を指定したプロファイルから参照できるようにする

--- cmd/root.go
+++ cmd/root.go
@@ -11,15 +11,116 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/iam"
 	"github.com/aws/aws-sdk-go-v2/service/sts"
 	"github.com/aws/aws-sdk-go-v2/service/sts/types"
+	"github.com/charmbracelet/bubbles/list"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 	"github.com/spf13/cobra"
 )
 
 var profile string
 var outProfile string
 var serialNumber string
+var selectSerialNumber string
 var durationSeconds int32
+var updateSerialNumber bool
+
+const ABORT_SELECT_SERIAL_NUMBER = "ABORT_SELECT_SERIAL_NUMBER"
+const NOT_FOUND_SERIAL_NUMBER = "NOT_FOUND_SERIAL_NUMBER"
+
+var docStyle = lipgloss.NewStyle().Margin(1, 2)
+
+type item struct {
+	deviceName string
+	arn        string
+}
+
+func (i item) Title() string       { return i.deviceName }
+func (i item) Description() string { return i.arn }
+func (i item) FilterValue() string { return i.deviceName }
+
+type model struct {
+	list list.Model
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		{
+			if msg.String() == "ctrl+c" {
+				selectSerialNumber = ABORT_SELECT_SERIAL_NUMBER
+				return m, tea.Quit
+			}
+			if msg.String() == "enter" {
+				i, ok := m.list.SelectedItem().(item)
+				if ok {
+					selectSerialNumber = i.arn
+					return m, tea.Quit
+				}
+			}
+		}
+	case tea.WindowSizeMsg:
+		{
+			h, v := docStyle.GetFrameSize()
+			m.list.SetSize(msg.Width-h, msg.Height-v)
+		}
+	}
+
+	var cmd tea.Cmd
+	m.list, cmd = m.list.Update(msg)
+	return m, cmd
+}
+
+func (m model) View() string {
+	return docStyle.Render(m.list.View())
+}
+
+// IAMユーザーの仮想MFAデバイス一覧を取得して、シリアル番号を１つ選択する
+func getVirtualMfaDevice(ctx context.Context, cfg aws.Config) string {
+	iamClient := iam.NewFromConfig(cfg)
+	resp, err := iamClient.ListVirtualMFADevices(ctx, &iam.ListVirtualMFADevicesInput{})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	if len(resp.VirtualMFADevices) == 0 {
+		// 仮想MFAデバイスが1つも登録されていない場合
+		return NOT_FOUND_SERIAL_NUMBER
+	}
+	if len(resp.VirtualMFADevices) == 1 {
+		return *resp.VirtualMFADevices[0].SerialNumber
+	}
+
+	// 仮想MFAデバイスが複数ある場合は選択肢を表示し選択してもらう
+	var devices []list.Item
+	for _, device := range resp.VirtualMFADevices {
+		deviceName := strings.Join(strings.Split((*device.SerialNumber), "/")[1:], "/")
+		devices = append(devices, item{
+			deviceName: deviceName,
+			arn:        *device.SerialNumber,
+		})
+	}
+	l := list.New(devices, list.NewDefaultDelegate(), 0, 0)
+	l.SetFilteringEnabled(false)
+	l.DisableQuitKeybindings()
+	l.InfiniteScrolling = true
+	m := model{list: l}
+	m.list.Title = "Select MFA Device"
+
+	p := tea.NewProgram(m, tea.WithAltScreen())
+
+	if _, err := p.Run(); err != nil {
+		fmt.Println("Error running program:", err)
+		os.Exit(1)
+	}
+
+	return selectSerialNumber
+}
 
 // 引数で指定されたプロファイルから認証情報を取得
 func loadAWSProfile(ctx context.Context) aws.Config {
@@ -31,17 +132,28 @@ func loadAWSProfile(ctx context.Context) aws.Config {
 		log.Fatalln(err)
 	}
 
-	// MFAのシリアル番号が指定されていない場合はAWS Configから取得
-	if serialNumber == "" {
-		outCfg, err := config.LoadSharedConfigProfile(ctx, outProfile)
-		if err != nil {
-			log.Fatalln(err)
+	// AWS ConfigからMFAのシリアル番号を取得
+	outCfg, err := config.LoadSharedConfigProfile(ctx, outProfile)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	serialNumber = outCfg.MFASerial
+
+	// AWS ConfigにMFAのシリアル番号が指定されていない場合はIAMユーザーの仮想MFAデバイスを取得する
+	if serialNumber == "" || updateSerialNumber {
+		serialNumber = getVirtualMfaDevice(ctx, cfg)
+
+		// 仮想デバイスの選択をキャンセルした場合はエラー
+		if serialNumber == ABORT_SELECT_SERIAL_NUMBER {
+			log.Fatalln("Selection of the virtual MFA device has been interrupted.")
 		}
-		serialNumber = outCfg.MFASerial
-		if serialNumber == "" {
-			log.Fatalln("There is no value for mfa_serial in the AWS configure specified in the --out-profile argument. Please specify the --serial-number argument.")
+
+		// 仮想デバイスが1つも登録されていない場合はエラー
+		if serialNumber == NOT_FOUND_SERIAL_NUMBER {
+			log.Fatalln("Virtual MFA device is not set up. Please log in to the AWS Management Console and configure a virtual MFA device.")
 		}
 	}
+
 	return cfg
 }
 
@@ -109,7 +221,7 @@ func Execute() {
 
 func init() {
 	rootCmd.Flags().StringVarP(&profile, "profile", "p", "default", "AWS profile name")
-	rootCmd.Flags().StringVarP(&serialNumber, "serial-number", "s", "", "MFA identifier. If not specified, the value of mfa_serial in AWS configure specified in the --out-profile argument is used.\nEx: arn:aws:iam::123456789012:mfa/user")
 	rootCmd.Flags().StringVarP(&outProfile, "out-profile", "o", "default-sts", "AWS profile name to be used in STS")
 	rootCmd.Flags().Int32VarP(&durationSeconds, "duration-seconds", "d", 3600, "The duration, in seconds, that the credentials should remain valid.")
+	rootCmd.Flags().BoolVarP(&updateSerialNumber, "update-serial-number", "u", false, "Update the MFA serial number")
 }
